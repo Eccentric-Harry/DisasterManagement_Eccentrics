77b3b1bd96f40930fcf9732baf61cd822968945b
Anantk13 <anant.kapooor@gmail.com>
2024-09-05 23:29:42 +0530

    css update 2

--- frontend/src/components/dashFooter.js
+++ frontend/src/components/dashFooter.js
@@ -1,37 +1,37 @@
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faHouse } from '@fortawesome/free-solid-svg-icons';
-// import { useLocation, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
-// const DashFooter = () => {
-//     const navigate = useNavigate();
-//     const { pathname } = useLocation;
+const DashFooter = () => {
+    const navigate = useNavigate();
+    const { pathname } = useLocation;
 
-//     const onGoHomeClicked = () => {
-//         navigate('/dash');
-//     };
+    const onGoHomeClicked = () => {
+        navigate('/dash');
+    };
 
-//     let goHomeButton = null;
+    let goHomeButton = null;
 
-//     if(pathname !== '/dash') {
-//         goHomeButton = (
-//             <button className="dash-footer__home" onClick={onGoHomeClicked}>
-//                 <FontAwesomeIcon icon={faHouse} title='Home' />
-//             </button>
-//         );
-//     };
+    if(pathname !== '/dash') {
+        goHomeButton = (
+            <button className="dash-footer__home" onClick={onGoHomeClicked}>
+                <FontAwesomeIcon icon={faHouse} title='Home' />
+            </button>
+        );
+    };
 
-//     const content = (
-//         <footer className="dash-footer">
-//             {goHomeButton}
-//             <div className="dash-footer__container">
-//                 <p style={{color:'teal'}}>Current User:</p>
-//                 <p style={{color:'teal'}}>Status:</p>
-//                 <p style={{color:'teal'}}>&copy; 2024</p>
-//             </div>
-//         </footer>
-//     );
-//   return content;
-// };
+    const content = (
+        <footer className="dash-footer">
+            {goHomeButton}
+            <div className="dash-footer__container">
+                <p style={{color:'teal'}}>Current User:</p>
+                <p style={{color:'teal'}}>Status:</p>
+                <p style={{color:'teal'}}>&copy; 2024</p>
+            </div>
+        </footer>
+    );
+  return content;
+};
 
-// export default DashFooter;
+export default DashFooter;
--- frontend/src/components/dashLayout.js
+++ frontend/src/components/dashLayout.js
@@ -1,17 +1,17 @@
-// import { Outlet } from "react-router-dom";
-// // import DashHeader from "./dashHeader";
-// // import DashFooter from "./dashFooter";
+import { Outlet } from "react-router-dom";
+// import DashHeader from "./dashHeader";
+// import DashFooter from "./dashFooter";
 
-// const DashLayout = ({ children }) => {
-//     return (
-//         <>
-//             {/* <DashHeader /> */}
-//             <div className="dash-container">
-//                 <Outlet />
-//             </div>
-//             {/* <DashFooter /> */}
-//         </>
-//     );
-// };
+const DashLayout = ({ children }) => {
+    return (
+        <>
+            {/* <DashHeader /> */}
+            <div className="dash-container">
+                <Outlet />
+            </div>
+            {/* <DashFooter /> */}
+        </>
+    );
+};
 
-// export default DashLayout;
+export default DashLayout;
